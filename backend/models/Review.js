2c650278447ed3d6ec903de870f27ef5fa6c0cdd
tymairp96 <tymairp96@gmail.com>
2025-11-19 12:01:50 -0500

    Add Review model for MongoDB with artist info, rating, and comment

---
+++ backend/models/Review.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    artistName: {
+        type: String,
+        required: true,
+    },
+    artistImage: {
+        type: String, 
+        required: false,
+    },
+    albumName: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5,
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+        
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;

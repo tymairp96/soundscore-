88a68d36b2c4a12c8555ed0ea652273695189d60
tymairp96 <tymairp96@gmail.com>
2025-11-21 10:28:33 -0500

    Update

--- frontend/soundscore frontend/src/pages/AddReview.jsx
+++ frontend/soundscore frontend/src/pages/AddReview.jsx
@@ -4,6 +4,7 @@ function AddReview() {
 const [artistName, setArtistName] = useState("");
 const [albumName, setAlbumName] = useState("");
 const [songName, setSongName] = useState("");
+const [rating, setRating] = useState(1);
 
 
 }

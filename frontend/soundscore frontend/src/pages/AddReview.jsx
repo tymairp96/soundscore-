4ae6006038532ef6b36a71b2d798067325660fb4
tymairp96 <tymairp96@gmail.com>
2025-11-21 10:26:47 -0500

    AddReview

--- frontend/soundscore frontend/src/pages/AddReview.jsx
+++ frontend/soundscore frontend/src/pages/AddReview.jsx
@@ -2,6 +2,8 @@ import {useState} from "react";
 
 function AddReview() {
 const [artistName, setArtistName] = useState("");
+const [albumName, setAlbumName] = useState("");
+const [songName, setSongName] = useState("");
 
 
 }
